dcf089e5f0748a17655fc25170a83d2e98cd32be
wineboutique2023-glitch <wineboutique2023@gmail.com>
2026-05-02 16:27:15 +1000

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://你的project-id.supabase.co";
-const supabaseAnonKey = "你的anon key";
+const supabaseUrl = https://yubwwqcohdjtvvryvvzs.supabase.co/rest/v1/;
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1Ynd3cWNvaGRqdHZ2cnl2dnpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0OTQ0ODYsImV4cCI6MjA5MzA3MDQ4Nn0.CeckQyeDPySjb7bOU2Tmrn_mgNAzfVneTemwDNEEDdE;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
