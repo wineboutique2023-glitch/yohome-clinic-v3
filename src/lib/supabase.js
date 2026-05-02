d0d64291532c2208917fcf694c84ee1f8ad3f496
wineboutique2023-glitch <wineboutique2023@gmail.com>
2026-05-02 16:33:16 +1000

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = https://yubwwqcohdjtvvryvvzs.supabase.co/rest/v1/;
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1Ynd3cWNvaGRqdHZ2cnl2dnpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0OTQ0ODYsImV4cCI6MjA5MzA3MDQ4Nn0.CeckQyeDPySjb7bOU2Tmrn_mgNAzfVneTemwDNEEDdE;
+const supabaseUrl = "https://yubwwqcohdjtvvryvvzs.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1Ynd3cWNvaGRqdHZ2cnl2dnpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0OTQ0ODYsImV4cCI6MjA5MzA3MDQ4Nn0.CeckQyeDPySjb7bOU2Tmrn_mgNAzfVneTemwDNEEDdE";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
